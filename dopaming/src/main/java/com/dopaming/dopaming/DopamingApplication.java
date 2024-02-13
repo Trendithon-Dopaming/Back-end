2fc9489fa3d21036556ea7393073723797448b60
se0hyun <kimlisa00@seoultech.ac.kr>
2024-02-13 18:29:11 +0900

    first commit

---
+++ dopaming/src/main/java/com/dopaming/dopaming/DopamingApplication.java
@@ -0,0 +1,13 @@
+package com.dopaming.dopaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopamingApplication.class, args);
+	}
+
+}
