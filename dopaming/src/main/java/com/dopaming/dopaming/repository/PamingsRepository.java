2ce7a6d99958b1d140345cc9eb6fc0363412df18
se0hyun <kimlisa00@seoultech.ac.kr>
2024-02-22 20:21:11 +0900

    FEAT: 팜 목록 조회, 상세조회, 생성, 수정 기능 구현

---
+++ dopaming/src/main/java/com/dopaming/dopaming/repository/PamingsRepository.java
@@ -0,0 +1,9 @@
+package com.dopaming.dopaming.repository;
+
+import com.dopaming.dopaming.domain.Pamings;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PamingsRepository extends JpaRepository<Pamings, Long> {
+
+}
