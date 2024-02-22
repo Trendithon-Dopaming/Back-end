52600fe33901c0719cdf4aae9345d587234bfd24
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-02-23 00:51:19 +0900

    feat: add RootController

---
+++ dopaming/src/main/java/com/dopaming/dopaming/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.dopaming.dopaming.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/health")
+    public String HealthCheck() {
+        return "I'm Healthy";
+    }
+}
