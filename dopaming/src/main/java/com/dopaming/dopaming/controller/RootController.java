76578f9b831eb8626a9d11f8a0a8b4bcc92b9a51
오유은 <122276414+yueunfive@users.noreply.github.com>
2024-02-23 16:24:19 +0900

    Update RootController.java

--- dopaming/src/main/java/com/dopaming/dopaming/controller/RootController.java
+++ dopaming/src/main/java/com/dopaming/dopaming/controller/RootController.java
@@ -2,6 +2,7 @@ package com.dopaming.dopaming.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class RootController {
